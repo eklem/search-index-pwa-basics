a444725eff9deb689d778232631b019cef72ca3c
Espen Klem <espen.klem@gmail.com>
2021-03-14 13:24:10 +0100

    Just the basic project setup

---
+++ dist/indexWorker.js
@@ -0,0 +1,3 @@
+// indexWorke.js
+// Proxy for all things search-index create, update and delete part of CRUD
+// This is done as a service worker since it's the most async part of a search engine.
---
+++ dist/queryWorker.js
@@ -0,0 +1,3 @@
+// queryWorke.js
+// Proxy for all things search-index query side. Read part of CRUD
+// This is done as a web worker since indexing is using a service worker
